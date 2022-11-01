bf3f216633bef8c2c51c4d9cbad7d1c361cab6c9
Kolya132 <nikovsienko14@gmail.com>
2022-11-01 17:09:39 +0300

    init save(commit)

---
+++ Javascript1hw/hw1.js
@@ -0,0 +1,44 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+const q = 'hello'
+console.log(q)
+const w = 'owu'
+console.log(w)
+const v = 'com'
+console.log(v)
+const d = 'ua'
+console.log(d)
+const num1 = 1
+console.log(num1)
+const num2 = 10
+console.log(num2)
+const num3 = -999
+console.log(num3)
+const num4 = 123
+console.log(num4)
+const num5 = 3.14
+console.log(num5)
+const num6 = 2.7
+console.log(num6)
+const num7 = 16
+console.log(num7)
+const bul1 = 5<6
+console.log(bul1)
+const bul2=6<5
+console.log(bul2)
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+const firstName = 'Kolya'
+const  midleName = 'Ovsienko'
+const lastName = 'Mykolaiovich'
+const person = firstName+midleName+lastName
+console.log(person)
+// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+let a = 100
+console.log(typeof a)
+let b = '100'
+console.log(typeof b)
+let c = true
+console.log(typeof c)
+console.log(prompt('your name please'))
+console.log(prompt('your father`s name please'))
+console.log(prompt('your age please'))
