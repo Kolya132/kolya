0fdec7ec41bef856e63a9f51bbcbc974cf44b158
Kolya132 <nikovsienko14@gmail.com>
2022-11-03 17:09:02 +0300

    init save(commit)

---
+++ Javascript2hw/hw2.js
@@ -0,0 +1,52 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+const arr = [12,'qwew','1sw4',true,2>3,'deid',2.3,'qqq','aaa','bbb']
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+console.log(arr[4])
+console.log(arr[5])
+console.log(arr[6])
+console.log(arr[7])
+console.log(arr[8])
+console.log(arr[9])
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+const firstBook={
+    title:'Harry Potter',
+    pageCount: 100,
+    genre:'fantastic',
+    authors:[{name:'Petya',age:26}]
+}
+const secondBook={
+    title:'war and peace',
+    pageCount:1000,
+    genre:'Novel',
+    authors:[{name:'Sasha',age:'40'},{name:'Roma',age:54}]
+}
+const thirdBook={
+    title:'Buratino',
+    pageCount:300,
+    genre:'Comedy',
+    authors:[{name:'Anton',age:22},{name:'Dima',age:34}]
+}
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+let users=[{name: 'petya',username: 'wws',password: '2w28dha'},
+    {name: 'zhora',username: 'asaw',password:'aswhd'},
+    {name: 'dima',username: 'wdwds',password:'wdwd34rf'},
+    {name: 'anton',username: 'ede',password: 'cece3ew'},
+    {name: 'roma',username: 'ccbfv',password: 'eece12d'},
+    {name: 'artem',username: 'verv',password: 'evijr47yf'},
+    {name: 'vsnya',username: 'dehud',password: 'ug437'},
+    {name: 'valera',username: 'uefe',password: '3d3dhcu4'},
+    {name: 'kolya',username: 'dh4fg',password: 'djevnx23a'},
+    {name: 'youra',username: 'uec',password: 'becb2e'}]
+console.log(users[0].password)
+console.log(users[1].password)
+console.log(users[2].password)
+console.log(users[3].password)
+console.log(users[4].password)
+console.log(users[5].password)
+console.log(users[6].password)
+console.log(users[7].password)
+console.log(users[8].password)
+console.log(users[9].password)
